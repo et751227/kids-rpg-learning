ff45bc259890ae400788093ab284d307192f6323
et751227 <36990569+et751227@users.noreply.github.com>
2025-05-01 11:54:04 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RPGWordGame from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RPGWordGame />
+  </React.StrictMode>
+)
