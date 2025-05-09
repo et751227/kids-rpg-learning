0ce1ce8122e5efcf84be5c8422e974393da3e6ef
et751227 <36990569+et751227@users.noreply.github.com>
2025-05-09 16:59:44 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css'; // Tailwind 樣式
+import PracticeMode from './PracticeMode.jsx'; // 這是你首頁想呈現的內容
 
-import PracticeMode from './PracticeMode';
-import ChallengeMode from './ChallengeMode';
-import RecordsPage from './pages/RecordsPage'; // ✅ 新增這行
+import './index.css'; // Tailwind 樣式
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PracticeMode />} />
-        <Route path="/challenge" element={<ChallengeMode />} />
-        <Route path="/records" element={<RecordsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <PracticeMode />
   </React.StrictMode>
 );
