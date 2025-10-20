bd1fec9ea2bc438a2f18f680deb9290c29b2293d
et751227 <36990569+et751227@users.noreply.github.com>
2025-10-20 11:47:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WorldMap from "./pages/WorldMap";
 import PracticeMode from "./pages/PracticeMode";
