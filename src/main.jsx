d3ea08e7d63e47f24ad513f43d34914f70701b9d
et751227 <36990569+et751227@users.noreply.github.com>
2025-10-20 11:25:06 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PracticeMode from './PracticeMode.jsx'; // 這是你首頁想呈現的內容
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import './index.css'; // Tailwind 樣式
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PracticeMode />
+    <App />
   </React.StrictMode>
 );
