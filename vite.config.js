829ec0cbdae4e78add0549def789c48504ba21e3
et751227 <36990569+et751227@users.noreply.github.com>
2025-10-20 13:30:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 這樣就可以了！
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 🔹 確保靜態資源可正確載入
-  build: {
-    minify: "terser", // 🔹 指定用 terser 取代 esbuild 壓縮器
-    terserOptions: {
-      mangle: {
-        // 🔹 保留這些關鍵詞，避免被壓縮成 undefined
-        reserved: ["HashRouter", "BrowserRouter", "Routes", "Route", "React"],
-      },
-    },
-  },
+  base: "./", 
 });
