c3b5956b82f9b7add04f6c8201d2d844885e3bce
et751227 <36990569+et751227@users.noreply.github.com>
2025-10-20 11:51:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: './', // ✅ 改成相對路徑，確保在 Vercel 上正確載入 JS/CSS
 });
